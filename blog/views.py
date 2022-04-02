96233f0500bf5cfed34c47bd5047ca6368181e7c
J Lee <ljinlabs@gmail.com>
2022-04-02 14:55:47 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+
+from blog.models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
